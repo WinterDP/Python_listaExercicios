8ea9cc896112767312a7c9179e89c3e7e2f446a2
João Prado <59054486+WinterDP@users.noreply.github.com>
2023-01-12 18:03:08 -0300

    desafio002 done

---
+++ desafio002.py
@@ -0,0 +1,8 @@
+'''
+Desafio 001
+Crie um programa que leia o nome de uma pessoa e mostre uma mensagem de boas vindas
+
+'''
+
+nome = input("Por favor, escreva o seu nome: ")
+print("Seja bem vindo!!!", nome)
